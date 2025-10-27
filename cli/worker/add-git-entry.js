29317c6ab7e70292507e45f5a4dbab3743c03ea6
gikkman <gikk@gikkman.com>
2025-10-27 16:15:03 +0100

    Fixing imports

--- cli/worker/add-git-entry.js
+++ cli/worker/add-git-entry.js
@@ -1,9 +1,9 @@
-const { processNewFile } = require('./git');
+const { processNewFile } = require('../../common/git');
 
 // Get the arguments passed from the parent process
 const [entryAbsolutePath, data] = JSON.parse(process.argv[2]);
 
-// Run the addEntry function
+// Run the processNewFile function
 processNewFile(entryAbsolutePath, data)
   .then(() => {
     process.exit(0);
